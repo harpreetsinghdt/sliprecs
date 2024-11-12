c6d3fe96fe192cea8caf60c71e8b1fbda91de849
Harpreet Singh <harpreetsinghdt@gmail.com>
2024-11-12 04:48:37 -0500

    new line  added

--- src/components/auth/Header.jsx
+++ src/components/auth/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
 
           <Offcanvas show={show} onHide={handleClose} placement="end">
             <Offcanvas.Header closeButton>
-              <Offcanvas.Title>FaamBill Menu</Offcanvas.Title>
+              <Offcanvas.Title>FaamBill </Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="flex-column">
