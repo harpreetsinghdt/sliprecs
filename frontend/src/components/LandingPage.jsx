41e39ca4b1bfcae8bd7e405d7acac134cb1b4845
Harpreet Singh <harpreetsinghdt@gmail.com>
2024-11-19 17:48:59 -0500

    changes

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -67,7 +67,7 @@ const LandingPage = () => {
           </Link>
         </div>
       </section>
-      testing content to auto build try again sd dsfd 55555 11111
+      testing content to auto build try again sd dsfd 55555 11111 dsad
       {/* Footer */}
       <footer className="text-center py-4 bg-light">
         <p>&copy; 2024 sliprecs. All Rights Reserved.</p>
