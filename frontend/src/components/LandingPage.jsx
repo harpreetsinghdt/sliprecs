9768bc825384cb2da5c12c40edb1b93b81bfab99
Harpreet Singh <harpreetsinghdt@gmail.com>
2024-11-20 19:03:42 -0500

    new code update

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -15,7 +15,7 @@ const LandingPage = () => {
             Get Started
           </a>
         </div>
-        5555555 456464 dsfsd
+        5555555 11111
       </section>  
       {/* Features Section */}
       <section id="features" className="feature-section">
