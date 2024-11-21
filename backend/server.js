bf6cfe44191439e5db208506cae34825117598d1
Harpreet Singh <harpreetsinghdt@gmail.com>
2024-11-20 20:32:38 -0500

    static file serve  path

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 const mongoose = require("mongoose");
 const apiRoutes = require("./routes/api");
 
