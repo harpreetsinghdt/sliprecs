598431132bf0b95cbbc1e75d628ebdfcd0bd14e0
Harpreet Singh <harpreetsinghdt@gmail.com>
2024-11-21 15:22:59 -0500

    logger

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -23,7 +23,7 @@ const upload = multer({ storage: storage });
 
 // Define an example GET route
 router.get("/", (req, res) => {
-  res.json({ message: "Welcome to the nodejs + express backend API.21 nov" });
+  res.json({ message: "Welcome to the nodejs + express backend API.21 nov 2024" });
 });
 
 // Define an example GET route
