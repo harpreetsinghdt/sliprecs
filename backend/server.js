8f715c3c5b6cc597a506797db887ec9a63140a13
Harpreet Singh <harpreetsinghdt@gmail.com>
2024-11-20 22:33:52 -0500

    production backend env

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ const apiRoutes = require("./routes/api");
 
 // Load environment variables from .env file
 dotenv.config();
+const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env";
+dotenv.config({ path: envFile });
 
 // Initialize Express app
 const app = express();
