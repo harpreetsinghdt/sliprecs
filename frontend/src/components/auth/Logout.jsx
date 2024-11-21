27d699875403ca52bec72304e660e6e75b87e536
Harpreet Singh <harpreetsinghdt@gmail.com>
2024-11-20 20:07:25 -0500

    logout notify added

--- frontend/src/components/auth/Logout.jsx
+++ frontend/src/components/auth/Logout.jsx
@@ -1,5 +1,11 @@
 import { Navigate } from "react-router-dom";
 import axios from "axios";
+import {
+  notifySuccess,
+  notifyError,
+  notifyInfo,
+  notifyWarning,
+} from "../utils/toastConfig";
 
 const Logout = () => {
   // Remove the token from storage
@@ -7,6 +13,8 @@ const Logout = () => {
 
   // Remove the token from Axios headers
   delete axios.defaults.headers.common["Authorization"];
+
+  notifySuccess("Logged out successfully.");
 
   // Redirect to login page
   return <Navigate to="/login" />;
