854eb5ba5c631fdb27e0db1fa477918f44294be7
Harpreet Singh <harpreetsinghdt@gmail.com>
2024-11-27 18:45:53 -0500

    modify changes

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -12,7 +12,7 @@ const LandingPage = () => {
             Effortlessly save and organize your receipts in one place!
           </p>
           <a href="#cta" className="cta-btn">
-            Get Started 4234 1111
+            Get Started 4234 1111 hfg
           </a>
         </div>
         
