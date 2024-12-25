5ea0257c182987cb77f799d5ef71543c2daf0279
Harpreet Singh <harpreetsinghdt@gmail.com>
2024-12-25 15:42:08 -0500

    new changes

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -12,7 +12,7 @@ const LandingPage = () => {
             Effortlessly save and organize your receipts in one place!
           </p>
           <a href="#cta" className="cta-btn"> 
-            Get Starte dfsd sdfd
+            Get Starte dfsd sdfd fgffs
           </a> 
         </div>
       </section>
