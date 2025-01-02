2f227997c20ed39b255cf91ab6f685f2cb75d788
Harpreet Singh <harpreetsinghdt@gmail.com>
2025-01-02 16:18:24 -0500

    Happy New Year 2025

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -13,7 +13,7 @@ const LandingPage = () => {
           </p>
           <a href="#cta" className="cta-btn"> 
             Lets Get Started Happy New Year
-            Have a successful year ahead 2025 123456
+            Have a successful year ahead 2025 1234567
           </a> 
         </div>
       </section>
