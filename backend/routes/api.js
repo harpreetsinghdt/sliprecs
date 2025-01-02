1d9c0bcb11db522ff90ae03c789bd12cd5cb1225
Harpreet Singh <harpreetsinghdt@gmail.com>
2025-01-02 14:51:27 -0500

    Happy New Year 2025

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -24,7 +24,7 @@ const upload = multer({ storage: storage });
 // Define an example GET route
 router.get("/", (req, res) => {
   res.json({
-    message: "Welcome to the nodejs + express backend API. fdsdfrs21 nov 2024",
+    message: "Welcome to the nodejs + express backend API. Happy New Year 2025",
   });
 });
 
--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -12,7 +12,7 @@ const LandingPage = () => {
             Effortlessly save and organize your receipts in one place!
           </p>
           <a href="#cta" className="cta-btn"> 
-            Lets Get Started 2025 Yahhoo
+            Lets Get Started Happy New Year
           </a> 
         </div>
       </section>
