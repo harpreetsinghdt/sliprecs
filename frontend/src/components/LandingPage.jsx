7ed69b96ed8de1b932637217ebef7a7551c3a0dc
Harpreet Singh <harpreetsinghdt@gmail.com>
2025-01-03 14:55:58 -0500

    new day

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -12,7 +12,7 @@ const LandingPage = () => {
             Effortlessly save and organize your receipts in one place!
           </p>
           <a href="#cta" className="cta-btn"> 
-            Lets Get Start new year with new goals
+            Lets Get Start new year with new goals, cheers yahhoo
           </a> 
         </div>
       </section>
