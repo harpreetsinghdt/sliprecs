2b87e5e313b750328741f09179822a55b4d40396
Harpreet Singh <harpreetsinghdt@gmail.com>
2025-01-03 17:26:27 -0500

    new day

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,7 @@ module.exports = {
         NODE_ENV: "production",
         PORT: 5000,
         MONGO_URI: "mongodb://localhost:27017/sliprecs",
+        JWT_SECRET:"millionaire"
       },
     },
   ],
