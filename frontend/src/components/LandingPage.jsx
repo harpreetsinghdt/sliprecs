1139709e0258aa58c4370ddfc8ea7c57361fca1c
Harpreet Singh <harpreetsinghdt@gmail.com>
2025-01-03 14:09:11 -0500

    new day

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -12,7 +12,7 @@ const LandingPage = () => {
             Effortlessly save and organize your receipts in one place!
           </p>
           <a href="#cta" className="cta-btn"> 
-            Lets Get Started df dsj dfsdf
+            Lets Get Started df dsj dfsdf dsfs
           </a> 
         </div>
       </section>
